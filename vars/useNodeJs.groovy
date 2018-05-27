26c05471441d61b6458581245563789cfe8d9b77
István Antal <i@istvan.xyz>
2018-05-27 21:14:51 +0100

    Add useNodeJs

---
+++ vars/useNodeJs.groovy
@@ -0,0 +1,21 @@
+def call(String nodeVersion, String npmVersion = '') {
+    def platform = 'linux-x64'
+    if (fetchCommandOutput('uname').contains('Darwin')) {
+        platform = 'darwin-x64'    
+    }
+
+    def nodeJsToolDir = "${env.JENKINS_HOME}/../tools/nodejs"
+    def binPath = "${nodeJsToolDir}/node-v${nodeVersion}-${platform}/bin"
+    env.PATH = "${binPath}:${env.PATH}"
+
+    if (!fileExists(nodeJsToolDir)) {
+        sh "mkdir ${nodeJsToolDir}"
+    }
+
+    if (!fileExists(binPath)) {
+        sh "cd ${nodeJsToolDir}; curl -LO \"https://nodejs.org/dist/v${nodeVersion}/node-v${npmVersion}-${platform}.tar.gz\""
+        sh "cd ${nodeJsToolDir}; tar xvf \"node-v${nodeVersion}-${platform}.tar.gz\""
+        sh "cd ${nodeJsToolDir}; rm \"node-v${nodeVersion}-${platform}.tar.gz\""
+        sh "PATH=\"${binPath}:\$PATH\" npm i -g \"npm@${npmVersion}\";"
+    }
+}
